63aef3302f7f1cd3deac676f60ccdcf06fa8acfd
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-02-09 11:49:11 +0200

    Add cors polices

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 const {connect_db} = require('./util/db');
 const globalErrorHandler = require('./Controllers/errorController');
 const userRouter = require('./Routs/userRout');
 
+app.use(cors())
 connect_db()
 app.use(morgan("dev"));
 app.use(express.json());
@@ -15,7 +17,7 @@ app.use(globalErrorHandler);
 app.use('/api/', userRouter);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 })
