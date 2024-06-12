db5f0cbce4c5d74b183d4dd888a85fda6d664970
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-06-12 12:01:14 +0300

    Update notification route

--- Routs/notificationRouts.js
+++ Routs/notificationRouts.js
@@ -6,7 +6,7 @@ const protect = require('../util/middlewares');
 router.get('/get-notifications', protect(['client', 'guide']),  notificationController.getAllNotifications);
 router.get('/get-notifications-count', protect(['client', 'guide']),  notificationController.getNotificationCount);
 router.post('/create-notification', protect(['client', 'guide']),  notificationController.createNotification);
-router.post('/delete-notification', protect(['client', 'guide']),  notificationController.deleteNotification);
+router.get('/delete-notification/:notificationId', protect(['client', 'guide']),  notificationController.deleteNotification);
 router.post('/delete-all-notifications', protect(['client', 'guide']),  notificationController.deleteAllNotifications);
 
 module.exports = router;
