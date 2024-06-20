f502f7ad556eb138e8c75974e67a4607197a287e
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-06-20 22:27:34 +0300

    Add update tour guide data

--- Routs/guideRouts.js
+++ Routs/guideRouts.js
@@ -8,7 +8,7 @@ router.get('/get-all-guides', guideController.getTourGuides);
 router.get('/guide/:id', protect(['client', 'guide', 'admin']) , guideController.getGuide);
 router.get('/guide-tours/:guideId' , guideController.guideTours);
 router.get('/accept-application' , guideController.acceptApplication) 
-router.patch('/update-guide' , guideController.updateGuide) // not finished yet
+router.patch('/update-guide',protect(['guide']) , guideController.updateGuide) // not finished yet
 router.delete('/delete-guide-account' , protect(['guide']) , guideController.deleteGuideAccount);
 router.delete('/delete-guide/:guideId' , protect(['admin']) , guideController.deleteGuide);
 
