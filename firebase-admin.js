d7fb5d71bb77f0bfc4c47c897e1df10dd1aefb9f
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-06-20 10:16:20 +0300

    Dockerize application and add firebase file to project

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = require('./triplash-b7e57-firebase-adminsdk-lrkv8-0bf39d1c06.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
